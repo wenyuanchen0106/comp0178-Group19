77aa541537bf64afd93fd32976941fbb920cfe7e
haowensheng6-eng <haowensheng6@gmail.com>
2025-11-29 23:18:19 +0000

    Show uploaded images on browse page

--- auction/browse.php
+++ auction/browse.php
@@ -2,12 +2,13 @@
 require_once 'utilities.php';
 close_expired_auctions();
 activate_pending_auctions();
-// 先给三个变量一个默认值，避免上面的表单里 echo 未定义变量
+
+// Initialize filter variables from GET for the search form
 $keyword  = isset($_GET['keyword'])   ? trim($_GET['keyword'])   : '';
 $category = isset($_GET['cat'])       ? $_GET['cat']             : 'all';
 $ordering = isset($_GET['order_by'])  ? $_GET['order_by']        : 'pricelow';
 
-// ========= 新增：从数据库读取所有类别 =========
+// Load all categories for the dropdown
 $categories = [];
 $sql_cat = "SELECT category_id, category_name FROM categories ORDER BY category_name";
 $result_cat = db_query($sql_cat);
@@ -18,11 +19,8 @@ if ($result_cat instanceof mysqli_result) {
     $result_cat->free();
 }
 
-include_once 'header.php'; 
-
-  
+include_once 'header.php';
 ?>
- 
 
 <div class="container">
 
@@ -43,14 +41,14 @@ include_once 'header.php';
               <i class="fa fa-search"></i>
             </span>
           </div>
-             <input
-                type="text"
-                class="form-control border-left-0"
-                id="keyword"
-                name="keyword"
-                placeholder="Search for anything"
-                value="<?php echo htmlspecialchars($keyword); ?>"
-              >
+          <input
+            type="text"
+            class="form-control border-left-0"
+            id="keyword"
+            name="keyword"
+            placeholder="Search for anything"
+            value="<?php echo htmlspecialchars($keyword); ?>"
+          >
         </div>
       </div>
     </div>
@@ -59,16 +57,16 @@ include_once 'header.php';
         <label for="cat" class="sr-only">Search within:</label>
         <select class="form-control" id="cat" name="cat">
           <option value="all" <?php if ($category === 'all') echo 'selected'; ?>>
-                All categories
-              </option>
-              <?php foreach ($categories as $cat_row): ?>
-                <option
-                  value="<?php echo $cat_row['category_id']; ?>"
-                  <?php if ((string)$category === (string)$cat_row['category_id']) echo 'selected'; ?>
-                >
-                  <?php echo htmlspecialchars($cat_row['category_name']); ?>
-                </option>
-              <?php endforeach; ?>
+            All categories
+          </option>
+          <?php foreach ($categories as $cat_row): ?>
+            <option
+              value="<?php echo $cat_row['category_id']; ?>"
+              <?php if ((string)$category === (string)$cat_row['category_id']) echo 'selected'; ?>
+            >
+              <?php echo htmlspecialchars($cat_row['category_name']); ?>
+            </option>
+          <?php endforeach; ?>
         </select>
       </div>
     </div>
@@ -76,15 +74,15 @@ include_once 'header.php';
       <div class="form-inline">
         <label class="mx-2" for="order_by">Sort by:</label>
         <select class="form-control" id="order_by" name="order_by">
-              <option value="pricelow" <?php if ($ordering === 'pricelow') echo 'selected'; ?>>
-                Price (low to high)
-              </option>
-              <option value="pricehigh" <?php if ($ordering === 'pricehigh') echo 'selected'; ?>>
-                Price (high to low)
-              </option>
-              <option value="date" <?php if ($ordering === 'date') echo 'selected'; ?>>
-                Soonest expiry
-              </option>
+          <option value="pricelow" <?php if ($ordering === 'pricelow') echo 'selected'; ?>>
+            Price (low to high)
+          </option>
+          <option value="pricehigh" <?php if ($ordering === 'pricehigh') echo 'selected'; ?>>
+            Price (high to low)
+          </option>
+          <option value="date" <?php if ($ordering === 'date') echo 'selected'; ?>>
+            Soonest expiry
+          </option>
         </select>
       </div>
     </div>
@@ -95,202 +93,226 @@ include_once 'header.php';
 </form>
 </div> <!-- end search specs bar -->
 
-
 </div>
 
 <?php
-  // Retrieve these from the URL
-  if (!isset($_GET['keyword'])) {
-    // TODO: Define behavior if a keyword has not been specified.
-    $keyword = '';  
-  }
-  else {
-    $keyword = trim($_GET['keyword']);
-  }
+// Retrieve filters again (this block is from the starter code)
+if (!isset($_GET['keyword'])) {
+  $keyword = '';
+} else {
+  $keyword = trim($_GET['keyword']);
+}
 
-  if (!isset($_GET['cat'])) {
-    $category = 'all';
-    // TODO: Define behavior if a category has not been specified.
-  }
-  else {
-    $category = $_GET['cat'];
-  }
-  
-  if (!isset($_GET['order_by'])) {
-    $ordering = 'pricelow';
-    // TODO: Define behavior if an order_by value has not been specified.
-  }
-  else {
-    $ordering = $_GET['order_by'];
-  }
-  
-  if (!isset($_GET['page'])) {
+if (!isset($_GET['cat'])) {
+  $category = 'all';
+} else {
+  $category = $_GET['cat'];
+}
+
+if (!isset($_GET['order_by'])) {
+  $ordering = 'pricelow';
+} else {
+  $ordering = $_GET['order_by'];
+}
+
+if (!isset($_GET['page'])) {
+  $curr_page = 1;
+} else {
+  $curr_page = (int)$_GET['page'];
+  if ($curr_page < 1) {
     $curr_page = 1;
   }
-  else {
-    $curr_page = (int)$_GET['page'];
-    if ($curr_page < 1) {
-      $curr_page = 1;
-    }
-  }
+}
 
-  /* TODO: Use above values to construct a query. Use this query to 
-     retrieve data from the database. (If there is no form data entered,
-     decide on appropriate default value/default query to make. */
-  
-  /* For the purposes of pagination, it would also be helpful to know the
-     total number of results that satisfy the above query */
-  // ============= 构造查询条件 =============
+/* TODO: Use above values to construct a query. Use this query to 
+   retrieve data from the database. (If there is no form data entered,
+   decide on appropriate default value/default query to make. */
 
-  // 基本条件：只显示 active 的拍卖
-  $where  = " WHERE a.status = 'active' ";
-  $params = [];
-  $types  = '';
+/* For the purposes of pagination, it would also be helpful to know the
+   total number of results that satisfy the above query */
 
-  // 关键词：在 title 和 description 中模糊搜索
-  if ($keyword !== '') {
-    $where .= " AND (i.title LIKE ? OR i.description LIKE ?) ";
-    $like = '%' . $keyword . '%';
-    $params[] = $like;
-    $params[] = $like;
-    $types   .= 'ss';
-  }
+// Build WHERE clause for active auctions only
+$where  = " WHERE a.status = 'active' ";
+$params = [];
+$types  = '';
 
-  // 分类：如果不是 all，就按 category_id 过滤
-  if ($category !== 'all' && $category !== '') {
-    $where .= " AND i.category_id = ? ";
-    $params[] = (int)$category;
-    $types   .= 'i';
-  }
+// Keyword filter (search in title and description)
+if ($keyword !== '') {
+  $where .= " AND (i.title LIKE ? OR i.description LIKE ?) ";
+  $like = '%' . $keyword . '%';
+  $params[] = $like;
+  $params[] = $like;
+  $types   .= 'ss';
+}
 
-  // 排序方式
-  // pricelow / pricehigh / date
-  $order_by_sql = " ORDER BY ";
-  switch ($ordering) {
-    case 'pricehigh':
-      $order_by_sql .= "current_price DESC";
-      break;
-    case 'date':
-      $order_by_sql .= "a.end_date ASC";
-      break;
-    case 'pricelow':
-    default:
-      $order_by_sql .= "current_price ASC";
-      break;
-  }
- // 每页多少条
-  $results_per_page = 10;
- // ============= 先算总结果数（用于分页） =============
-  $count_sql = "
-    SELECT COUNT(DISTINCT a.auction_id) AS total
-    FROM auctions a
-    JOIN items i ON a.item_id = i.item_id
-    $where
-  ";
+// Category filter (if not "all")
+if ($category !== 'all' && $category !== '') {
+  $where .= " AND i.category_id = ? ";
+  $params[] = (int)$category;
+  $types   .= 'i';
+}
 
-  $result_count = db_query($count_sql, $types, $params);
-  $row_count    = $result_count->fetch_assoc();
-  $num_results  = (int)$row_count['total'];
-  $result_count->free();
+// Ordering for results
+$order_by_sql = " ORDER BY ";
+switch ($ordering) {
+  case 'pricehigh':
+    $order_by_sql .= "current_price DESC";
+    break;
+  case 'date':
+    $order_by_sql .= "a.end_date ASC";
+    break;
+  case 'pricelow':
+  default:
+    $order_by_sql .= "current_price ASC";
+    break;
+}
 
-  if ($num_results == 0) {
-    $max_page = 1;
-  } else {
-    $max_page = ceil($num_results / $results_per_page);
-  }
+// Pagination settings
+$results_per_page = 10;
 
-  if ($curr_page > $max_page) {
-    $curr_page = $max_page;
-  }
+// Count total results for pagination
+$count_sql = "
+  SELECT COUNT(DISTINCT a.auction_id) AS total
+  FROM auctions a
+  JOIN items i ON a.item_id = i.item_id
+  $where
+";
 
-  $offset = ($curr_page - 1) * $results_per_page;
+$result_count = db_query($count_sql, $types, $params);
+$row_count    = $result_count->fetch_assoc();
+$num_results  = (int)$row_count['total'];
+$result_count->free();
 
-  // ============= 再查当前这一页的具体数据 =============
-  $list_sql = "
-    SELECT 
-      a.auction_id,
-      a.item_id,
-      i.title,
-      i.image_path,
-      i.description,
-      COALESCE(MAX(b.bid_amount), a.start_price) AS current_price,
-      COUNT(b.bid_id) AS num_bids,
-      a.end_date
-    FROM auctions a
-    JOIN items i ON a.item_id = i.item_id
-    LEFT JOIN bids b ON a.auction_id = b.auction_id
-    $where
-    GROUP BY a.auction_id
-    $order_by_sql
-    LIMIT ? OFFSET ?
-  ";
+if ($num_results == 0) {
+  $max_page = 1;
+} else {
+  $max_page = ceil($num_results / $results_per_page);
+}
 
-  // 在原参数后面加上 LIMIT / OFFSET
-  $list_params = $params;
-  $list_types  = $types . 'ii';
-  $list_params[] = $results_per_page;
-  $list_params[] = $offset;
+if ($curr_page > $max_page) {
+  $curr_page = $max_page;
+}
 
-  $result_list = db_query($list_sql, $list_types, $list_params);
+$offset = ($curr_page - 1) * $results_per_page;
+
+// Query actual listings for the current page
+$list_sql = "
+  SELECT 
+    a.auction_id,
+    a.item_id,
+    i.title,
+    i.image_path,
+    i.description,
+    COALESCE(MAX(b.bid_amount), a.start_price) AS current_price,
+    COUNT(b.bid_id) AS num_bids,
+    a.end_date
+  FROM auctions a
+  JOIN items i ON a.item_id = i.item_id
+  LEFT JOIN bids b ON a.auction_id = b.auction_id
+  $where
+  GROUP BY a.auction_id
+  $order_by_sql
+  LIMIT ? OFFSET ?
+";
+
+// Append LIMIT / OFFSET params
+$list_params   = $params;
+$list_types    = $types . 'ii';
+$list_params[] = $results_per_page;
+$list_params[] = $offset;
+
+$result_list = db_query($list_sql, $list_types, $list_params);
 ?>
 
 <div class="container mt-5">
 
-<!-- TODO: If result set is empty, print an informative message. Otherwise... -->
-
 <ul class="list-group">
 
-<!-- TODO: Use a while loop to print a list item for each auction listing
-     retrieved from the query -->
-
-
 <?php
-  // Demonstration of what listings will look like using dummy data.
- /* $item_id = "87021";
-  $title = "Dummy title";
-  $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
-  $current_price = 30;
-  $num_bids = 1;
-  $end_date = new DateTime('2020-09-16T11:00:00');
-  
-  // This uses a function defined in utilities.php
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-  
-  $item_id = "516";
-  $title = "Different title";
-  $description = "Very short description.";
-  $current_price = 13.50;
-  $num_bids = 3;
-  $end_date = new DateTime('2020-11-02T00:00:00');
-  
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-*/
- // 如果没有结果
-  if ($num_results == 0) {
-    echo '<li class="list-group-item">No results found.</li>';
-  }
-  else {
-    // 用老师给的 print_listing_li()，循环真实数据
-    while ($row = $result_list->fetch_assoc()) {
-      $item_id       = $row['item_id'];                // 先沿用 item_id 版本
-      $title         = $row['title'];
-      $description   = $row['description'];
-      $current_price = (float)$row['current_price'];
-      $num_bids      = (int)$row['num_bids'];
-      $end_date      = new DateTime($row['end_date']);
+// If no results, show message
+if ($num_results == 0) {
+  echo '<li class="list-group-item">No results found.</li>';
+} else {
+  $now = new DateTime();
 
-      print_listing_li(
-        $item_id,
-        $title,
-        $description,
-        $current_price,
-        $num_bids,
-        $end_date,
-        $row['image_path']
-      );
+  // Loop through each auction and render HTML, including thumbnail image
+  while ($row = $result_list->fetch_assoc()) {
+    $item_id       = (int)$row['item_id'];
+    $title         = $row['title'];
+    $description   = $row['description'];
+    $current_price = (float)$row['current_price'];
+    $num_bids      = (int)$row['num_bids'];
+    $end_date      = new DateTime($row['end_date']);
+    $image_path    = $row['image_path'];
+
+    // Compute time remaining text
+    $time_remaining = '';
+    if ($now < $end_date) {
+      $diff = date_diff($now, $end_date);
+      $time_remaining = display_time_remaining($diff) . ' remaining';
+    } else {
+      $time_remaining = 'Ended';
     }
-    $result_list->free();
+
+    // Truncate description for list view
+    $short_desc = $description;
+    if (strlen($short_desc) > 140) {
+      $short_desc = substr($short_desc, 0, 137) . '...';
+    }
+
+    // Start list item
+    ?>
+    <li class="list-group-item"
+        style="background-color: #111; border: 1px solid #333; margin-bottom: 10px; border-radius: 4px;">
+      <div class="row align-items-center">
+
+        <div class="col-md-2">
+          <?php
+          // If an image exists, show it; otherwise show the Stark placeholder box
+          if (!empty($image_path) && file_exists($image_path)) {
+            echo '<img src="' . htmlspecialchars($image_path) . '" alt="Item image" class="img-fluid"
+                     style="width: 140px; height: 140px; object-fit: cover; border-radius: 4px;">';
+          } else {
+            // Fallback div uses the original CSS "IMG PENDING" background
+            echo '<div class="img-placeholder-sm" style="width: 140px; height: 140px;"></div>';
+          }
+          ?>
+        </div>
+
+        <div class="col-md-7">
+          <h5 class="mb-1">
+            <a href="listing.php?item_id=<?php echo $item_id; ?>"
+               class="text-light"
+               style="font-family: \'Oswald\', sans-serif; letter-spacing: 0.5px;">
+              <?php echo htmlspecialchars($title); ?>
+            </a>
+          </h5>
+          <p class="mb-1 text-muted" style="font-size: 0.9rem;">
+            <?php echo htmlspecialchars($short_desc); ?>
+          </p>
+          <small class="text-warning">
+            <?php echo $end_date->format('j M H:i'); ?><?php echo $time_remaining ? ' · ' . $time_remaining : ''; ?>
+          </small>
+        </div>
+
+        <div class="col-md-3 text-right">
+          <div style="font-size: 1.5rem; font-weight: bold; color: var(--color-accent);">
+            £<?php echo number_format($current_price, 2); ?>
+          </div>
+          <div class="text-muted small mb-2">
+            <?php echo $num_bids; ?> bid(s)
+          </div>
+          <a href="listing.php?item_id=<?php echo $item_id; ?>" class="btn btn-outline-warning btn-sm">
+            VIEW
+          </a>
+        </div>
+
+      </div>
+    </li>
+    <?php
   }
+  $result_list->free();
+}
 ?>
 
 </ul>
@@ -298,9 +320,7 @@ include_once 'header.php';
 <!-- Pagination for results listings -->
 <nav aria-label="Search results pages" class="mt-5">
   <ul class="pagination justify-content-center">
-  
 <?php
-
   // Copy any currently-set GET variables to the URL.
   $querystring = "";
   foreach ($_GET as $key => $value) {
@@ -308,12 +328,12 @@ include_once 'header.php';
       $querystring .= "$key=$value&amp;";
     }
   }
-  
+
   $high_page_boost = max(3 - $curr_page, 0);
-  $low_page_boost = max(2 - ($max_page - $curr_page), 0);
-  $low_page = max(1, $curr_page - 2 - $low_page_boost);
-  $high_page = min($max_page, $curr_page + 2 + $high_page_boost);
-  
+  $low_page_boost  = max(2 - ($max_page - $curr_page), 0);
+  $low_page        = max(1, $curr_page - 2 - $low_page_boost);
+  $high_page       = min($max_page, $curr_page + 2 + $high_page_boost);
+
   if ($curr_page != 1) {
     echo('
     <li class="page-item">
@@ -323,25 +343,19 @@ include_once 'header.php';
       </a>
     </li>');
   }
-    
+
   for ($i = $low_page; $i <= $high_page; $i++) {
     if ($i == $curr_page) {
-      // Highlight the link
-      echo('
-    <li class="page-item active">');
+      echo('<li class="page-item active">');
+    } else {
+      echo('<li class="page-item">');
     }
-    else {
-      // Non-highlighted link
-      echo('
-    <li class="page-item">');
-    }
-    
-    // Do this in any case
+
     echo('
       <a class="page-link" href="browse.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
     </li>');
   }
-  
+
   if ($curr_page != $max_page) {
     echo('
     <li class="page-item">
@@ -352,13 +366,9 @@ include_once 'header.php';
     </li>');
   }
 ?>
-
   </ul>
 </nav>
 
-
 </div>
 
-
-
-<?php include_once("footer.php")?>
+<?php include_once("footer.php"); ?>
