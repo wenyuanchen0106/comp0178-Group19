a9ee7134cd6f6dac8469ee07fbe9034cadeb55a9
charlestang1104-hue <charlestang1104@gmail.com>
2025-11-30 02:55:54 +0000

    Add admin report dismissal functionality
    Signed-off-by: charlestang1104-hue <charlestang1104@gmail.com>

---
+++ auction/admin_report_dismiss.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'utilities.php';
+require_login();
+if ($_SESSION['role_id'] != 3) die("Access denied.");
+
+$id = intval($_GET['id']);
+
+db_execute("UPDATE reports SET status='dismissed' WHERE report_id = ?", "i", [$id]);
+
+header("Location: admin_review_reports.php");
+exit;
