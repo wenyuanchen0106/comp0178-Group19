745cb265f7b156b3375dc1a918516a0084bd2e7b
charlestang1104-hue <charlestang1104@gmail.com>
2025-11-30 18:27:59 +0000

    241

--- auction/pay_result.php
+++ auction/pay_result.php
@@ -1,5 +1,5 @@
 <?php
-// Enable error reporting for debugging during development
+// 1Enable error reporting for debugging during development
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
